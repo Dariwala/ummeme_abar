f94c9aef3495b5201fe4a61fe7ccff3ebbf24347
dariwala <daeeshehab@gmail.com>
2019-10-12 06:13:54 +0600

    A little bug in previous commit so fixed it

--- app/Http/Controllers/PhoneEmailIcon.php
+++ app/Http/Controllers/PhoneEmailIcon.php
@@ -1,55 +1,31 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Redirect;
-
 use Session;
-
-
 class PhoneEmailIcon
 {
     public static function handlePhoneandEmail($str,$is_bangla,$b_str)
     {
         $matches = array();
-
         
-
         if($is_bangla == TRUE)
         {
             // returns all results in array $matches
             preg_match_all('/[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}/', $b_str, $matches);
             $matches = $matches[0];
-
             foreach($matches as $match)
             {
                 $b_str = str_replace($match,$match.'<a href="mailto:'.$match.'" style="color:black;"><i class="fa fa-envelope" style="margin-left:5px;"></i></a>',$b_str);
             }
-
             $matches = array();
-
             // returns all results in array $matches
             preg_match_all('/\+88-{0,1}01[0-9]{3}-{0,1}[0-9]{6}/', $str, $matches);
             $matches = $matches[0];
-
             foreach($matches as $match)
             {
                 $b_str = str_replace(BanglaConverter::en2bn($match),BanglaConverter::en2bn($match).'<a href="tel:'.$match.'" style="color:black;"><i class="fa fa-phone" style="margin-left:5px;"></i></a>',$b_str);
             }
-
-            // returns all results in array $matches -hotline
-            /*preg_match_all('/: [0-9]+/', $str, $matches);
-            $matches = $matches[0];
-            foreach($matches as $match)
-            {
-                $h_number = substr($match, 2);
-                $b_str = str_replace(BanglaConverter::en2bn($match),BanglaConverter::en2bn($match).'<a href="tel:'.$h_number.'" style="color:black;"><i class="fa fa-phone" style="margin-left:5px;display: inline-block;
-                border-radius:60px;
-                border:1px solid black;
-                padding:0.5em 0.6em;"></i></a>',$b_str);
-            }*/
             return $b_str;
         }
         else
@@ -57,13 +33,11 @@ class PhoneEmailIcon
             // returns all results in array $matches - email
             preg_match_all('/[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}/', $str, $matches);
             $matches = $matches[0];
-
             foreach($matches as $match)
             {
                 $str = str_replace($match,$match.'<a href="mailto:'.$match.'" style="color:black;"><i class="fa fa-envelope" style="margin-left:5px;"></i></a>',$str);
             }
             $matches = array();
-
             // returns all results in array $matches
             preg_match_all('/\+88-{0,1}01[0-9]{3}-{0,1}[0-9]{6}/', $str, $matches);
             $matches = $matches[0];
@@ -71,22 +45,8 @@ class PhoneEmailIcon
             {
                 $str = str_replace($match,$match.'<a href="tel:'.$match.'" style="color:black;"><i class="fa fa-phone" style="margin-left:5px;"></i></a>',$str);
             }
-            
-            // returns all results in array $matches
-            /*preg_match_all('/: [0-9]+/', $str, $matches);
-            $matches = $matches[0];
-            foreach($matches as $match)
-            {
-                $h_number = substr($match, 2);
-                $str = str_replace($match,$match.'<a href="tel:'.$h_number.'" style="color:black;"><i class="fa fa-phone" style="margin-left:5px;display: inline-block;
-                border-radius:60px;
-                border:1px solid black;
-                padding:0.5em 0.6em;"></i></a>',$str);
-            }*/
             return $str;
-
         }
-
         
     }
 }
