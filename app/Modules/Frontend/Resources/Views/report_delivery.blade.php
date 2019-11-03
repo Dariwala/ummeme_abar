9a517c647f27f643551d7bdce46dc887b2c4c6a1
dariwala <daeeshehab@gmail.com>
2019-11-03 21:55:29 +0600

    https://www.facebook.com/100005271256073/videos/1268589083326788/ report delivery page ui changed. box instead of line

--- app/Modules/Frontend/Resources/Views/report_delivery.blade.php
+++ app/Modules/Frontend/Resources/Views/report_delivery.blade.php
@@ -51,6 +51,19 @@
         .uk-tab-bn>li>a{
             font-size: 15px !important;
         }
+        .txta{
+            width:99.7%;
+            min-height:100px;
+            font-family: 'Helvetica', Arial, Lucida Grande, sans-serif;
+            resize:none;
+            overflow:hidden;
+            vertical-align:top;
+            line-height:1.4;
+            height:20px;
+            min-height:20px;
+            padding-top:6px;
+            padding-left:6px;
+        }
     </style>
     
 @endsection
@@ -74,16 +87,16 @@
                                         <h4></h4>
                                         <div class="uk-width-1-1 uk-margin-top">
                                             <div class="parsley-row">
-                                                <label for="name">নাম <span class="req">*</span></label>
-                                                <textarea type="text" id="name" name="name" value="" required class="md-input" ></textarea>
+                                                <label for="name">নাম <span class="req">*</span></label><br>
+                                                <input type="text" id="name" name="name" value="" required style="width:calc(100% - 8px);height:25px;padding-left:6px;"></input><br><br>
                                             </div>
                                             <div class="parsley-row uk-margin-top">
-                                                <label for="email">ঠিকানা <span class="req">*</span></label>
-                                                <textarea type="text" id="address" name="address" value="" required class="md-input"></textarea>
+                                                <label for="email">ঠিকানা <span class="req">*</span></label><br>
+                                                <textarea type="text" id="address" name="address" value="" required class="txta" style="width:calc(100% - 8px);font-family: 'Helvetica', Arial, Lucida Grande, sans-serif;resize:none;overflow:hidden;vertical-align:top;line-height:1.4;height:20px;min-height:20px;padding-top:6px;padding-left:6px;"></textarea><br><br>
                                             </div>
                                             <div class="parsley-row uk-margin-top">
-                                                <label for="subject">যোগাযোগের নম্বর <span class="req">*</span></label>
-                                                <textarea type="text" id="phone_number" name="phone_number" value="" required class="md-input"></textarea>
+                                                <label for="subject">যোগাযোগের নম্বর <span class="req">*</span></label><br>
+                                                <input type="text" id="phone_number" name="phone_number" value="" required style="width:calc(100% - 8px);height:25px;padding-left:6px;"></input>
                                             </div>
                                             <br>
                                             <div class="uk-width-medium-1-2">
@@ -98,7 +111,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="uk-float-right uk-margin-top">
+                                <div class="uk-float-right uk-margin-top" style="margin-left:6px;">
                                     <button type="submit" class="md-btn md-btn-large" style="background: #FD0100;color: #fff;">পাঠিয়ে দিন</button>
                                 </div>
                             {!! Form::close() !!}
@@ -125,16 +138,16 @@
                                         <h4></h4>
                                         <div class="uk-width-1-1 uk-margin-top">
                                             <div class="parsley-row">
-                                                <label for="name">Name <span class="req">*</span></label>
-                                                <textarea type="text" id="name" name="name" value="" required class="md-input" ></textarea>
+                                                <label for="name">Name <span class="req">*</span></label><br>
+                                                <input type="text" id="name" name="name" value="" required style="width:calc(100% - 8px);height:25px;padding-left:6px;"></input><br><br>
                                             </div>
                                             <div class="parsley-row uk-margin-top">
-                                                <label for="email">Address<span class="req">*</span></label>
-                                                <textarea type="text" id="address" name="address" value="" required class="md-input" ></textarea>
+                                                <label for="email">Address<span class="req">*</span></label><br>
+                                                <textarea type="text" id="address" name="address" class="txta" value="" required style="width:calc(100% - 8px);font-family:'Helvetica', Arial, Lucida Grande, sans-serif;resize:none;overflow:hidden;vertical-align:top;line-height:1.4;height:20px;min-height:20px;padding-top:6px;padding-left:6px;"></textarea><br><br>
                                             </div>
                                             <div class="parsley-row uk-margin-top">
-                                                <label for="subject">Contact Number <span class="req">*</span></label>
-                                                <textarea type="text" id="phone_number" name="phone_number" value="" required class="md-input" ></textarea>
+                                                <label for="subject">Contact Number <span class="req">*</span></label><br>
+                                                <input type="text" id="phone_number" name="phone_number" value="" required style="width:calc(100% - 8px);height:25px;padding-left:6px;"></input>
                                             </div>
                                             <br>
                                             <div class="uk-width-medium-1-2">
@@ -161,6 +174,74 @@
     @endif
 
     <input id="backbuttonstate" type="text" value="0" style="display:none;" />
+    <script type="text/javascript">
+        // Targets all textareas with class "txta"
+        let textareas = document.querySelectorAll('.txta'),
+        hiddenDiv = document.createElement('div'),
+        content = null;
+
+        // Adds a class to all textareas
+        for (let j of textareas) {
+        j.classList.add('txtstuff');
+        }
+
+        // Build the hidden div's attributes
+
+        // The line below is needed if you move the style lines to CSS
+        // hiddenDiv.classList.add('hiddendiv');
+
+        // Add the "txta" styles, which are common to both textarea and hiddendiv
+        // If you want, you can remove those from CSS and add them via JS
+        hiddenDiv.classList.add('txta');
+        //hiddenDiv.style.width = '100%';
+
+        // Add the styles for the hidden div
+        // These can be in the CSS, just remove these three lines and uncomment the CSS
+        hiddenDiv.style.display = 'none';
+        hiddenDiv.style.whiteSpace = 'pre-wrap';
+        hiddenDiv.style.wordWrap = 'break-word';
+
+        // Loop through all the textareas and add the event listener
+        for(let i of textareas) {
+        (function(i) {
+            // Note: Use 'keyup' instead of 'input'
+            // if you want older IE support
+            i.addEventListener('input', function() {
+            
+            // Append hiddendiv to parent of textarea, so the size is correct
+            i.parentNode.appendChild(hiddenDiv);
+            
+            // Remove this if you want the user to be able to resize it in modern browsers
+            i.style.resize = 'none';
+            
+            // This removes scrollbars
+            i.style.overflow = 'hidden';
+
+            // Every input/change, grab the content
+            content = i.value;
+
+            // Add the same content to the hidden div
+            
+            // This is for old IE
+            content = content.replace(/\n/g, '<br>');
+            
+            // The <br ..> part is for old IE
+            // This also fixes the jumpy way the textarea grows if line-height isn't included
+            hiddenDiv.innerHTML = content;
+
+            // Briefly make the hidden div block but invisible
+            // This is in order to read the height
+            hiddenDiv.style.visibility = 'hidden';
+            hiddenDiv.style.display = 'block';
+            i.style.height = hiddenDiv.offsetHeight + 'px';
+
+            // Make the hidden div display:none again
+            hiddenDiv.style.visibility = 'visible';
+            hiddenDiv.style.display = 'none';
+            });
+        })(i);
+        }
+    </script>
     <script>
     document.addEventListener('DOMContentLoaded', function () {
     var ibackbutton = document.getElementById("backbuttonstate");
